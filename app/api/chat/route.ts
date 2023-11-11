52cefbfbb8e78cbe95017b258de90ecdb62a6809
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-11-11 12:41:22 -0600

    si

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,36 +1,30 @@
 import { messagesStr, bodyImportant } from "@/app/type"
 import { NextResponse, NextRequest } from "next/server"
-import { HfInference } from '@huggingface/inference';
+import { HfInferenceEndpoint } from '@huggingface/inference';
 import { experimental_buildOpenAssistantPrompt } from 'ai/prompts';
 
 // Create a new HuggingFace Inference instance
 
-const Hf = new HfInference(process.env.TOKEN,{
-          // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
-
-  url: process.env.ENDPOINT
-});
+const Hf = new HfInferenceEndpoint(process.env.ENDPOINT!,process.env.TOKEN);
 export async function POST(req: NextRequest) {
   let body: bodyImportant = await req.json()
-  if (process.env.TOKEN) {
-    const response = Hf.textGeneration({
-      
-      model: "",
-        // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+  const response = Hf.textGeneration({
 
-      inputs: experimental_buildOpenAssistantPrompt((body.msgs.slice(body.msgs.length-10))),
-      parameters: {
-        max_new_tokens: 200,
-        // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
-        typical_p: 0.2,
-        repetition_penalty: 1,
-        truncate: 1000,
-        return_full_text: false,
-      },
-    });
+    model: "",
+    // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
 
-    return NextResponse.json({ content: (await response as any).generated_text,role:"assistant"})
-  }
+    inputs: experimental_buildOpenAssistantPrompt((body.msgs.slice(body.msgs.length - 10))),
+    parameters: {
+      max_new_tokens: 200,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+      typical_p: 0.2,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false,
+    },
+  });
 
-  return NextResponse.json({ content: "system error", role: "system" })
+  return NextResponse.json({ content: (await response as any).generated_text, role: "assistant" })
+
+
 }
