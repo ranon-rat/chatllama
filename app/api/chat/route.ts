746e7b795b00c4d4e2182a12fdd8f945aaa2ee7f
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-11-14 23:16:30 -0600

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,14 @@
 import { messagesStr, bodyImportant } from "@/app/type"
 import { NextResponse, NextRequest } from "next/server"
 import { HfInferenceEndpoint } from '@huggingface/inference';
-import { experimental_buildOpenAssistantPrompt } from 'ai/prompts';
 
 // Create a new HuggingFace Inference instance
 
 const Hf = new HfInferenceEndpoint(process.env.ENDPOINT!,process.env.TOKEN);
+const systemPrompt=process.env.SYSTEM_PROMPT
 function toFormat(msgs:[messagesStr]){
+return   msgs.reduce((text,msg) => text +`<|${msg.role}|>\n${msg.content}\n</s>\n`, `<|system|>\n${systemPrompt}\n</s>\n`)+`<|assistant|>\n`;
+
 
 }
 export async function POST(req: NextRequest) {
@@ -16,8 +18,9 @@ export async function POST(req: NextRequest) {
     model: "",
     // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
 
-    inputs: experimental_buildOpenAssistantPrompt((body.msgs.slice(body.msgs.length - 10))),
+    inputs: toFormat(body.msgs.slice(-11)),
     parameters: {
+      temperature:0.5,
       max_new_tokens: 200,
       // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
       typical_p: 0.2,
